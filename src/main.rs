942a999ec028f14d5b6647591ecebc19875d3c02
ollipa <olli@paakkunainen.fi>
2021-02-04 13:31:31 +0900

    Fix scaling

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,10 @@ use bevy::prelude::*;
 const WINDOW_HEIGHT: f32 = 48.0;
 const WINDOW_WIDTH: f32 = 84.0;
 
-// const COLOR_LIGHT: (f32, f32, f32) = (0.78, 0.941, 0.847);
-// const COLOR_DARK: (f32, f32, f32) = (0.263, 0.322, 0.239);
+const COLOR_LIGHT: &str = "c7f0d8";
+const COLOR_DARK: &str = "43523d";
 
-struct Electron {
-    speed: f32,
-}
+struct Electron {}
 
 fn main() {
     App::build()
@@ -17,57 +15,46 @@ fn main() {
             width: WINDOW_WIDTH,
             height: WINDOW_HEIGHT,
             vsync: true,
+            scale_factor_override: Some(10.0),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb(0.78, 0.941, 0.847)))
+        .insert_resource(ClearColor(Color::hex(COLOR_LIGHT).unwrap()))
         .add_startup_system(setup.system())
         .add_system(electron_physics.system())
         .run();
 }
 
-fn setup(
-    commands: &mut Commands,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-    mut windows: ResMut<Windows>,
-) {
+fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands
         .spawn(UiCameraBundle::default())
         .spawn(OrthographicCameraBundle::new_2d())
+        // Electron
         .spawn(SpriteBundle {
-            material: materials.add(Color::rgb(0.263, 0.322, 0.239).into()),
+            material: materials.add(Color::hex(COLOR_DARK).unwrap().into()),
             transform: Transform::from_xyz(0.0, 0.0, 0.0),
             sprite: Sprite::new(Vec2::new(2.0, 2.0)),
             ..Default::default()
         })
-        .with(Electron { speed: 2.0 });
-
-    let window = windows.get_primary_mut().unwrap();
-    window.set_scale_factor_override(Some(10.0));
+        .with(Electron {});
 }
 
 fn electron_physics(
-    time: Res<Time>,
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Electron, &mut Transform)>,
+    mut query: Query<(&mut Transform, &Electron)>,
 ) {
-    for (electron, mut transform) in query.iter_mut() {
-        let mut direction = Vec2::new(0.0, 0.0);
-        if keyboard_input.pressed(KeyCode::Up) {
-            direction.y += 1.0;
+    for (mut transform, _) in query.iter_mut() {
+        if keyboard_input.just_pressed(KeyCode::Up) {
+            transform.translation.y += 1.0;
         }
-        if keyboard_input.pressed(KeyCode::Down) {
-            direction.y -= 1.0;
+        if keyboard_input.just_pressed(KeyCode::Down) {
+            transform.translation.y -= 1.0;
         }
-        if keyboard_input.pressed(KeyCode::Left) {
-            direction.x -= 1.0;
+        if keyboard_input.just_pressed(KeyCode::Left) {
+            transform.translation.x -= 1.0;
         }
-        if keyboard_input.pressed(KeyCode::Right) {
-            direction.x += 1.0;
+        if keyboard_input.just_pressed(KeyCode::Right) {
+            transform.translation.x += 1.0;
         }
-        direction.normalize();
-        let translation = &mut transform.translation;
-        translation.x += time.delta_seconds() * direction.x * electron.speed;
-        translation.y += time.delta_seconds() * direction.y * electron.speed;
     }
 }
