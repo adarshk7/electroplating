b6d601aad2774eec312b35cb504632aa59dc5ea1
Adarsh Krishnan <adarsh.krishnan@wolt.com>
2021-02-07 16:42:34 +0200

    Add basic sounds

--- src/plate.rs
+++ src/plate.rs
@@ -35,15 +35,24 @@ pub enum PlateState {
     Off,
 }
 
-pub fn plate_control_system(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Plate>) {
+pub fn plate_control_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Plate>,
+    asset_server: Res<AssetServer>,
+    audio: Res<Audio>,
+) {
     let mut plate_count = 1;
     let mut plate_selected = 1;
     for mut plate in query.iter_mut() {
         if plate.selected {
             if keyboard_input.just_pressed(KeyCode::Up) {
+                let music = asset_server.load("sound/blip5.wav");
+                audio.play(music);
                 plate.toggle();
             }
             if keyboard_input.just_pressed(KeyCode::Down) {
+                let music = asset_server.load("sound/blip3.wav");
+                audio.play(music);
                 plate.off();
             }
             plate_selected = plate.id;
@@ -52,12 +61,16 @@ pub fn plate_control_system(keyboard_input: Res<Input<KeyCode>>, mut query: Quer
         dbg!(&plate);
     }
     if keyboard_input.just_pressed(KeyCode::Left) {
+        let music = asset_server.load("sound/blip8.wav");
+        audio.play(music);
         plate_selected -= 1;
         if plate_selected < 1 {
             plate_selected = plate_count;
         }
     }
     if keyboard_input.just_pressed(KeyCode::Right) {
+        let music = asset_server.load("sound/blip9.wav");
+        audio.play(music);
         plate_selected += 1;
         if plate_selected > plate_count {
             plate_selected = 1;
