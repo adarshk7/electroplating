4edb5d8fe4fb844f84048b42c4ea19a4eb2e2f83
ollipa <olli@paakkunainen.fi>
2021-02-07 17:33:40 +0900

    Plate control and dummy physics

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,42 @@ const COLOR_DARK: &str = "43523d";
 #[derive(Debug)]
 struct Electron {}
 
+#[derive(Debug)]
+struct Plate {
+    id: usize,
+    state: PlateState,
+    selected: bool,
+}
+
+impl Plate {
+    fn new(id: usize) -> Self {
+        Plate {
+            id,
+            state: PlateState::Off,
+            selected: false,
+        }
+    }
+
+    fn toggle(&mut self) {
+        self.state = match self.state {
+            PlateState::Positive => PlateState::Negative,
+            PlateState::Negative => PlateState::Positive,
+            PlateState::Off => PlateState::Positive,
+        }
+    }
+
+    fn off(&mut self) {
+        self.state = PlateState::Off
+    }
+}
+
+#[derive(Debug)]
+enum PlateState {
+    Positive,
+    Negative,
+    Off,
+}
+
 fn main() {
     App::build()
         .insert_resource(WindowDescriptor {
@@ -25,6 +61,7 @@ fn main() {
         .insert_resource(ClearColor(Color::hex(COLOR_LIGHT).unwrap()))
         .add_startup_system(setup.system())
         .add_system(electron_physics.system())
+        .add_system(plate_control.system())
         .run();
 }
 
@@ -62,46 +99,129 @@ fn setup(commands: &mut Commands, mut materials: ResMut<Assets<ColorMaterial>>) 
             ..Default::default()
         })
         .spawn(SpriteBundle {
-            material: material_foreground,
+            material: material_foreground.clone(),
             transform: Transform::from_xyz(0.0, (WINDOW_HEIGHT - OUTER_WALL_THICKNESS) / 2.0, 0.0),
             sprite: Sprite::new(Vec2::new(WINDOW_WIDTH, OUTER_WALL_THICKNESS)),
             ..Default::default()
-        });
+        })
+        // Electric plates
+        .spawn(SpriteBundle {
+            material: material_foreground.clone(),
+            transform: Transform::from_xyz(
+                (WINDOW_WIDTH - OUTER_WALL_THICKNESS) / 2.0 - 1.0,
+                0.0,
+                0.0,
+            ),
+            sprite: Sprite::new(Vec2::new(1.0, 4.0)),
+            ..Default::default()
+        })
+        .with(Plate::new(1))
+        .spawn(SpriteBundle {
+            material: material_foreground,
+            transform: Transform::from_xyz(
+                0.0,
+                (WINDOW_HEIGHT - OUTER_WALL_THICKNESS) / 2.0 - 1.0,
+                0.0,
+            ),
+            sprite: Sprite::new(Vec2::new(4.0, 1.0)),
+            ..Default::default()
+        })
+        .with(Plate::new(2));
 }
 
 fn electron_physics(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&mut Transform, &Electron)>,
+    mut query_electron: Query<(&mut Transform, &Electron)>,
+    query_plates: Query<(&Transform, &Plate)>,
 ) {
-    for (mut transform, _) in query.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Up) {
-            transform.translation.y += 1.0;
+    for (mut electron_transform, _) in query_electron.iter_mut() {
+        let electron_loc = &mut electron_transform.translation;
+        for (plate_transform, plate) in query_plates.iter() {
+            let plate_loc = &plate_transform.translation;
+            match plate.state {
+                PlateState::Negative => {
+                    if electron_loc.x < plate_loc.x {
+                        electron_loc.x -= 1.0;
+                    } else if electron_loc.x > plate_loc.x {
+                        electron_loc.x += 1.0;
+                    }
+                    if electron_loc.y < plate_loc.y {
+                        electron_loc.y -= 1.0;
+                    } else if electron_loc.y > plate_loc.y {
+                        electron_loc.y += 1.0;
+                    }
+                }
+                PlateState::Positive => {
+                    if electron_loc.x < plate_loc.x {
+                        electron_loc.x += 1.0;
+                    } else if electron_loc.x > plate_loc.x {
+                        electron_loc.x -= 1.0;
+                    }
+                    if electron_loc.y < plate_loc.y {
+                        electron_loc.y += 1.0;
+                    } else if electron_loc.y > plate_loc.y {
+                        electron_loc.y -= 1.0;
+                    }
+                }
+                PlateState::Off => (),
+            }
         }
-        if keyboard_input.pressed(KeyCode::Down) {
-            transform.translation.y -= 1.0;
+        electron_bounds(&mut electron_transform);
+    }
+}
+
+/// Set electron bounds with outer walls.
+fn electron_bounds(transform: &mut Transform) {
+    transform.translation.y = transform
+        .translation
+        .y
+        .min((WINDOW_HEIGHT / 2.0) - OUTER_WALL_THICKNESS - ELECTRON_SIZE / 2.0);
+    transform.translation.y = transform
+        .translation
+        .y
+        .max((-WINDOW_HEIGHT / 2.0) + OUTER_WALL_THICKNESS + ELECTRON_SIZE / 2.0);
+    transform.translation.x = transform
+        .translation
+        .x
+        .min((WINDOW_WIDTH / 2.0) - OUTER_WALL_THICKNESS - ELECTRON_SIZE / 2.0);
+    transform.translation.x = transform
+        .translation
+        .x
+        .max((-WINDOW_WIDTH / 2.0) + OUTER_WALL_THICKNESS + ELECTRON_SIZE / 2.0);
+}
+
+fn plate_control(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Plate>) {
+    let mut plate_count = 1;
+    let mut plate_selected = 1;
+    for mut plate in query.iter_mut() {
+        if plate.selected {
+            if keyboard_input.just_pressed(KeyCode::Up) {
+                plate.toggle()
+            }
+            if keyboard_input.just_pressed(KeyCode::Down) {
+                plate.off()
+            }
+            plate_selected = plate.id;
         }
-        if keyboard_input.pressed(KeyCode::Left) {
-            transform.translation.x -= 1.0;
+        plate_count = plate_count.max(plate.id);
+        dbg!(&plate);
+    }
+    if keyboard_input.just_pressed(KeyCode::Left) {
+        plate_selected -= 1;
+        if plate_selected < 1 {
+            plate_selected = plate_count;
         }
-        if keyboard_input.pressed(KeyCode::Right) {
-            transform.translation.x += 1.0;
+    }
+    if keyboard_input.just_pressed(KeyCode::Right) {
+        plate_selected += 1;
+        if plate_selected > plate_count {
+            plate_selected = 1;
         }
-        // Set bounds with outer walls
-        transform.translation.y = transform
-            .translation
-            .y
-            .min((WINDOW_HEIGHT / 2.0) - OUTER_WALL_THICKNESS - ELECTRON_SIZE / 2.0);
-        transform.translation.y = transform
-            .translation
-            .y
-            .max((-WINDOW_HEIGHT / 2.0) + OUTER_WALL_THICKNESS + ELECTRON_SIZE / 2.0);
-        transform.translation.x = transform
-            .translation
-            .x
-            .min((WINDOW_WIDTH / 2.0) - OUTER_WALL_THICKNESS - ELECTRON_SIZE / 2.0);
-        transform.translation.x = transform
-            .translation
-            .x
-            .max((-WINDOW_WIDTH / 2.0) + OUTER_WALL_THICKNESS + ELECTRON_SIZE / 2.0);
+    }
+    for mut plate in query.iter_mut() {
+        if plate_selected == plate.id {
+            plate.selected = true;
+        } else {
+            plate.selected = false;
+        }
     }
 }
